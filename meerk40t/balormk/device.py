6d9d37595ccf9a9bdaac83304e12c79cd03c8c3e
Jacob Schmidt <schmidjw@gmail.com>
2023-02-12 23:06:38 -0500

    Make driver_install less dependent on my system directories.

--- meerk40t/balormk/device.py
+++ meerk40t/balormk/device.py
@@ -945,17 +945,19 @@ class BalorDevice(Service, ViewPort):
             for group in groups:
                 result[group[0]] = group[1]
             return result
+
+        @self.console_option("source_dir", "d", type=str, help="Directory where you have placed the drivers to install")
         @self.console_command(
             "driver_install",
             help=_("install driver"),
         )
-        def driver_install(command, channel, _, **kwgs):
+        def driver_install(command, channel, _, source_dir=None, **kwgs):
             import subprocess
             import ctypes
             vid = "9588"
             pid = "9899"
 
-            # Discover existing drivers assigned to specified VID/PID
+            # Find existing drivers assigned to specified VID/PID
             # This doesn't require elevated privileges
             list_process = r'pnputil.exe /enum-devices /deviceid USB\VID_{0}&PID_{1}'.format(vid, pid)
             p = subprocess.Popen(list_process, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
@@ -970,20 +972,22 @@ class BalorDevice(Service, ViewPort):
             # so that it just doesn't do the delete when the previous device doesn't exist
             if 'args' not in kwgs: return
 
+            # Existing driver found. Delete it using elevated privileges
             prev_driver = result['Driver Name']
-
-            # process = 'pnputil.exe /delete-driver {0} /force'.format(prev_driver)
-            # p = subprocess.Popen(process, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
-            # output = p.stdout.read()
-            # channel(output)
-
             ctypes.windll.shell32.ShellExecuteW(None, "runas", "pnputil.exe", '/delete-driver {0} /force'.format(prev_driver), None, 1)
 
+            # Massage source directory where the new drivers should be found
+            if source_dir is None:
+                source_dir = '%USERPROFILE%\\Downloads\\balor-drivers\\'
+            if not source_dir.endswith('\\'):
+                source_dir += '\\'
+
+            # Install the appropriately requested driver with elevated privileges.
             requested_driver = kwgs['args'][0]
             if requested_driver == "meerk40t":
-                args = '/add-driver {0} /install'.format(r"C:\Users\jacob\meerk40t-libusbK\MeerK40t_Balor.inf")
+                args = '/add-driver {0}{1} /install'.format(source_dir, "MeerK40t_Balor.inf")
             elif requested_driver == "ezcad":
-                args = '/add-driver {0} /install'.format(r"C:\Users\jacob\meerk40t-lmcv2u\Lmcv2u.inf")
+                args = '/add-driver {0}{1} /install'.format(source_dir, "Lmcv2u.inf")
             ctypes.windll.shell32.ShellExecuteW(None, "runas", "pnputil.exe", args, None, 1)
 
         @self.console_command(
